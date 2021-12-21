3c29428c8e65d6c1d7c35d2e9ab8a81c8d0526d7
leechungweii <tainanwilly@gmail.com>
2021-12-21 20:29:54 +0800

    first commit

---
+++ P32/src/P32.java
@@ -0,0 +1,13 @@
+
+public class P32 {
+
+	public static void main(String[] args )throws CarException {
+		// TODO Auto-generated method stub
+		Car car1;
+		car1 = new Car();
+		
+		car1.setCar(1234, -10.0);
+		car1.show();
+	}
+
+}
